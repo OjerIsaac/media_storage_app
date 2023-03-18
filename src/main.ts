57491700a99d5b39e6b394efb4558d5fef43b789
Ojer Isaac <isaacojerumu@gmail.com>
2023-03-18 02:15:23 +0100

    chores: project setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('NestApplication');
+
+  app.enableCors();
+
+  app.setGlobalPrefix('/api');
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('PORT');
+
+  await app.listen(port);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
